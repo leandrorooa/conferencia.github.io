a1faad1b4c97d2ce7ad8b02ce97446e439d4c100
Leandro Roo <leandrorooa@gmail.com>
2018-05-09 20:48:11 -0300

    commit
    commit
    
    commit
    
    commit
    
    commit
    
    commit
    
    commit
    
    commit
    
    commit
    
    commit
    
    commit
    
    commit
    
    commit
    
    commit
    
    commit
    
    commit
    
    co

--- js/main.js
+++ js/main.js
@@ -211,11 +211,15 @@ $(function() {
   });
 
   // ANIMACIONES A LOS NUMEROS
-
-  $('.resumen-evento li:nth-child(1) p').animateNumber({number: 6}, 1200);
-  $('.resumen-evento li:nth-child(2) p').animateNumber({number: 15}, 1200);
-  $('.resumen-evento li:nth-child(3) p').animateNumber({number: 3}, 1200);
-  $('.resumen-evento li:nth-child(4) p').animateNumber({number: 9}, 1200);
+  var resumenLista = jQuery('.resumen-evento');
+  if (resumenLista.length > 0) {
+    $('.resumen-evento').waypoint(function() {
+      $('.resumen-evento li:nth-child(1) p').animateNumber({number: 6}, 1200);
+      $('.resumen-evento li:nth-child(2) p').animateNumber({number: 15}, 1200);
+      $('.resumen-evento li:nth-child(3) p').animateNumber({number: 3}, 1200);
+      $('.resumen-evento li:nth-child(4) p').animateNumber({number: 9}, 1200);
+    }, {offset: '80%'});
+  }
 
   // CUENTA REGRESIVA
 
